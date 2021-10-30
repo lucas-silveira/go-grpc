f9c16ae0fec00425a38ca2713eef72a5129961d3
Lucas Silveira <lugano1995@gmail.com>
2021-10-30 15:28:47 -0300

    create cmd client go

---
+++ cmd/client/client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	pb "github.com/lucas-silveira/go-grpc/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Could not connect to gRPC Server: %v", err)
+	}
+
+	defer connection.Close()
+
+	client := pb.NewUserServiceClient(connection)
+	AddUser(client)
+}
+
+func AddUser(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "John",
+		Email: "john@snow.com",
+	}
+
+	res, err := client.AddUser(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Could not send gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
+}
