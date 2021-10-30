810d3aab98b38f3755fe1d8a4e2d20b7990628d0
Lucas Silveira <lugano1995@gmail.com>
2021-10-30 15:49:42 -0300

    implemented AddUserVerbose function in client.go

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	pb "github.com/lucas-silveira/go-grpc/pb"
@@ -19,7 +20,8 @@ func main() {
 	defer connection.Close()
 
 	client := pb.NewUserServiceClient(connection)
-	AddUser(client)
+	// AddUser(client)
+	AddUserVerbose(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -36,4 +38,32 @@ func AddUser(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "John",
+		Email: "john@snow.com",
+	}
+
+	resStream, err := client.AddUserVerbose(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Could not send gRPC request: %v", err)
+	}
+
+	for {
+		stream, err := resStream.Recv()
+
+		if err == io.EOF {
+			break // finish the loop
+		}
+
+		if err != nil {
+			log.Fatalf("Could not receive gRPC message: %v", err)
+		}
+
+		fmt.Println("Status:", stream.Status, " - ", stream.User)
+	}
 }
