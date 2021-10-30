a736717d0ceaaae7ef4f408c6afe31b83a2e0b79
Lucas Silveira <lugano1995@gmail.com>
2021-10-30 16:40:08 -0300

    implement AddUserBiStream function in client.go

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"sync"
 	"time"
 
 	pb "github.com/lucas-silveira/go-grpc/pb"
@@ -15,7 +16,7 @@ func main() {
 	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 
 	if err != nil {
-		log.Fatalf("Could not connect to gRPC Server: %v", err)
+		log.Fatalf("Could not connect to gRPC server: %v", err)
 	}
 
 	defer connection.Close()
@@ -23,7 +24,8 @@ func main() {
 	client := pb.NewUserServiceClient(connection)
 	// AddUser(client)
 	// AddUserVerbose(client)
-	AddUsers(client)
+	// AddUsers(client)
+	AddUserBiStream(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -102,7 +104,7 @@ func AddUsers(client pb.UserServiceClient) {
 	stream, err := client.AddUsers(context.Background())
 
 	if err != nil {
-		log.Fatalf("Could not send gRPC message stream: %v", err)
+		log.Fatalf("Could not connect to gRPC stream: %v", err)
 	}
 
 	for _, req := range reqs {
@@ -117,4 +119,76 @@ func AddUsers(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddUserBiStream(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		{
+			Id:    "1",
+			Name:  "John",
+			Email: "john@snow.com",
+		},
+		{
+			Id:    "2",
+			Name:  "John2",
+			Email: "john2@snow.com",
+		},
+		{
+			Id:    "3",
+			Name:  "John3",
+			Email: "john3@snow.com",
+		},
+		{
+			Id:    "4",
+			Name:  "John4",
+			Email: "john4@snow.com",
+		},
+		{
+			Id:    "5",
+			Name:  "John5",
+			Email: "john5@snow.com",
+		},
+	}
+
+	stream, err := client.AddUserBiStream(context.Background())
+
+	if err != nil {
+		log.Fatalf("Could not connect to gRPC stream: %v", err)
+	}
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		for _, req := range reqs {
+			fmt.Println("Sending user:", req.Name)
+			stream.Send(req)
+			time.Sleep(time.Second * 3)
+		}
+
+		stream.CloseSend()
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		for {
+			res, err := stream.Recv()
+
+			if err == io.EOF {
+				break
+			}
+
+			if err != nil {
+				log.Fatalf("Could not receive gRPC message: %v", err)
+				break
+			}
+
+			fmt.Printf("Receiving user %s with status %s\n", res.GetUser().GetName(), res.GetStatus())
+		}
+
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
